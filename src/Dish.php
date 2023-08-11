e24535836032cbe2b97a0814e5010208d31929ce
bool <zhenbeigg@163.com>
2023-08-11 09:19:18 +0800

    修改：请求方法

--- src/Dish.php
+++ src/Dish.php
@@ -3,7 +3,7 @@
  * @author: 布尔
  * @name:  菜品识别
  * @desc: 介绍
- * @LastEditTime: 2023-08-09 15:40:23
+ * @LastEditTime: 2023-08-11 08:15:00
  */
 namespace Eykj\Baidu;
 
@@ -32,9 +32,25 @@ class Dish
      */
     public function add(array $param) : array
     {
-        $r = $this->GuzzleHttp->post('https://aip.baidubce.com/rest/2.0/image-classify/v1/realtime_search/dish/add?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'image,brief'),['headers'=>['Content-Type'=> 'application/x-www-form-urlencoded']]);
+        $r = $this->GuzzleHttp->post('https://aip.baidubce.com/rest/2.0/image-classify/v1/realtime_search/dish/add?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'image,brief'), en_type: 'form_params');
         if (isset($r["error_code"])) {
-            error(500, $r['error_msg']);
+            switch ($r["error_code"]) {
+                case '216201':
+                    error(500, '图片格式错误,请上传PNG、JPG、JPEG、BMP格式图片');
+                    break;
+                
+                case '216203':
+                    error(500, '上传的图片中包含多个主体，请上传只包含一个主体的菜品图片');
+                    break;
+                
+                case '216200':
+                    error(500, '上传图片未检测到菜品');
+                    break;
+                
+                default:
+                    error(500, '添加失败');
+                    break;
+            }
         }
         return $r;
     }
@@ -47,9 +63,9 @@ class Dish
      */
     public function search(array $param) : array
     {
-        $r = $this->GuzzleHttp->post('https://aip.baidubce.com/rest/2.0/image-classify/v1/realtime_search/dish/search?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'image,url'),['headers'=>['Content-Type'=> 'application/x-www-form-urlencoded']]);
+        $r = $this->GuzzleHttp->post('https://aip.baidubce.com/rest/2.0/image-classify/v1/realtime_search/dish/search?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'image,url'), en_type: 'form_params');
         if (isset($r["error_code"])) {
-            error(500, $r['error_msg']);
+            error(500,'未检测到菜品信息');
         }
         return $r;
     }
@@ -62,9 +78,9 @@ class Dish
      */
     public function delete(array $param) : array
     {
-        $r = $this->GuzzleHttp->post('https://aip.baidubce.com/rest/2.0/image-classify/v1/realtime_search/dish/delete?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'image,url,cont_sign'),['headers'=>['Content-Type'=> 'application/x-www-form-urlencoded']]);
+        $r = $this->GuzzleHttp->post('https://aip.baidubce.com/rest/2.0/image-classify/v1/realtime_search/dish/delete?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'image,url,cont_sign'), en_type: 'form_params');
         if (isset($r["error_code"])) {
-            error(500, $r['error_msg']);
+            error(500,'删除失败');
         }
         return $r;
     }
