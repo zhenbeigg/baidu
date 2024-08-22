ff6b4edcb6557e15c4f848ac9374a84fa67ee6a1
bool <zhenbeigg@163.com>
2024-08-22 14:37:44 +0800

    修改：获取token方法

--- src/Service.php
+++ src/Service.php
@@ -5,6 +5,7 @@
  * @desc: 介绍
  * @LastEditTime: 2023-08-09 20:18:16
  */
+
 namespace Eykj\Baidu;
 
 use Eykj\Base\GuzzleHttp;
@@ -28,17 +29,17 @@ class Service
      */
     public function get_access_token(array $param): string
     {
-        if (!redis()->get('baidu_access_token')) {
+        if (!redis()->get($param['api_key'] . '_baidu_access_token')) {
             /* 获取配置url */
-            $r = $this->GuzzleHttp->get('https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id='.$param['api_key'].'&client_secret='. $param['secret_key']);
+            $r = $this->GuzzleHttp->get('https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=' . $param['api_key'] . '&client_secret=' . $param['secret_key']);
             if (isset($r["error"])) {
                 error(500, $r['error_description']);
             } else {
-                redis()->set('baidu_access_token', $r["access_token"], $r['expires_in']);
+                redis()->set($param['api_key'] . 'baidu_access_token', $r["access_token"], $r['expires_in']);
                 return $r["access_token"];
             }
         } else {
-            return redis()->get('baidu_access_token');
+            return redis()->get($param['api_key'] . 'baidu_access_token');
         }
     }
 }
